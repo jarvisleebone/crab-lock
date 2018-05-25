7b14565665bf280b6f3b7768f284d09e8ca01101
李想 <lixiang@zentech-inc.com>
2018-05-25 10:39:33 +0800

    init

---
+++ crab-demo/src/main/java/org/lock/demo/CrabDemoApplication.java
@@ -0,0 +1,12 @@
+package org.lock.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrabDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrabDemoApplication.class, args);
+    }
+}
---
+++ crab-zklock/src/main/java/org/crab/zklock/App.java
@@ -0,0 +1,13 @@
+package org.crab.zklock;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
